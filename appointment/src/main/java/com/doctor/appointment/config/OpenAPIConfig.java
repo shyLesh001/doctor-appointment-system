686afb3a80822d1a5f82b21ab3ab91e4b800d98b
syls <shaileshpillai2001@gmail.com>
2025-05-26 20:32:41 +0530

    Added Swagger UI support with OpenAPIConfig

---
+++ appointment/src/main/java/com/doctor/appointment/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.doctor.appointment.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Doctor Appointment System API")
+                        .version("1.0")
+                        .description("Interactive API documentation for managing doctors, patients, and appointments."));
+    }
+}
